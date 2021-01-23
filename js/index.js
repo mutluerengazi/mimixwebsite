9d2ebfb74c58d811434128b3673d9d1fbf147a6d
ender yazici <enderyazici57@gmail.com>
2021-01-23 19:48:57 +0300

    initial

---
+++ js/index.js
@@ -0,0 +1,5 @@
+AOS.init();
+particlesJS.load('particles-js', 'assets/particles.json', function() {});
+document.getElementById("accept").addEventListener("click", () => {
+    document.getElementById("cardie").classList.add("fade")
+})
