6872fc6acff6736224d6dc9db9af522685ade7d5
mutluerengazi <63504349+mutluerengazi@users.noreply.github.com>
2021-01-25 00:35:45 +0300

    updt
    asdasd

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
+if(localStorage.getItem("accept") != "true"){
+    document.querySelector("#cardie").style.display="inline"
+}
 AOS.init();
 particlesJS.load('particles-js', 'assets/particles.json', function() {});
 document.getElementById("accept").addEventListener("click", () => {
     document.getElementById("cardie").classList.add("fade")
+    document.querySelector("#cardie").style.display="none"
+    localStorage.setItem("accept","true");
 })
+console.log(localStorage.getItem("accept")=="true");
+
