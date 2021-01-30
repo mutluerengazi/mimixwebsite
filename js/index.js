342b00296254d19491191aee29b23cf8596d4720
mutluerengazi <63504349+mutluerengazi@users.noreply.github.com>
2021-01-30 15:03:30 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
-if(localStorage.getItem("accept") != "true"){
-    document.querySelector("#cardie").style.display="inline"
-}
-AOS.init();
-particlesJS.load('particles-js', 'assets/particles.json', function() {});
-document.getElementById("accept").addEventListener("click", () => {
-    document.getElementById("cardie").classList.add("fade")
-    document.querySelector("#cardie").style.display="none"
-    localStorage.setItem("accept","true");
-})
-console.log(localStorage.getItem("accept")=="true");
+$(document).ready(() => {
+    if(localStorage.getItem("accept") != "true"){
+        $("#cardie").css("display", "inline")
+    }
+    
+    AOS.init();
+    
+    particlesJS.load('particles-js', 'assets/particles.json', () => {});
+    
+    $("#accept").click(()=> {
+        $("#cardie").addClass("fade")
+        $("#cardie").css("display", "none")
+        localStorage.setItem("accept","true");
+    })
 
+    let scrollAnime = () => {
+        var v2 = Math.abs($('#bizeulasin').position().top - $(window).height()/2);
+        var v1 = $(this).scrollTop();
+            if( v1 > v2 ){
+                $(".img1").addClass("anim1")
+                $(".img2").addClass("anim2")
+                $(".img3").addClass("anim3")
+            }
+    }
+    
+    $(document).scroll((evt) => {
+        scrollAnime()
+    });
+    scrollAnime()
+});
